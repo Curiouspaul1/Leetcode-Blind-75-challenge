91c55177ee412affa5dbb8bc02298e04aa5d76b3
Oluwafemi <femio82@gmail.com>
2022-07-06 23:34:36 +0100

    a

--- _Enutrof_[__-1]🎧🌝💉☕/Day 1/two_sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dict_ ={}
-        for i,j in enumerate(nums):
-            compliment = target -j
-            if compliment not in dict_:
-                dict_[j] = i
-            else:
-                return [dict_[compliment], i]
