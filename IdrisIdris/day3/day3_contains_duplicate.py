3d4193b33d827467c9009b36b61f8f5115e01a87
Idris Idris <49335945+IdrisAdeyemi01@users.noreply.github.com>
2022-07-07 09:24:32 +0100

    d1-3

---
+++ IdrisIdris/day3/day3_contains_duplicate.py
@@ -0,0 +1,7 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        numSet = set(nums)
+        if len(numSet) != len(nums):
+            return True
+        else:
+            return False
