3886fffdb68ce6dcf35a8d6d097b2938092661a1
Oluwafemi <femio82@gmail.com>
2022-07-10 21:24:05 +0100

    a

--- Eff_Emm/product_of_array_except_itself.py
+++ Eff_Emm/product_of_array_except_itself.py
@@ -1,8 +1,16 @@
-from math import factorial, prod
-from functools import reduce
-nums = [1,2,3]
-before = 1
-dicts = {1:[2,3,4],2:[4,5,6]}
-arr = []
-prod =1
-print(dicts.values(0))
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        res = [1] * len(nums)
+        prefix= 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums)-1,-1,-1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
