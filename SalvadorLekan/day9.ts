26ce0f3272387183f2a8f6d097d21b77347f77e0
SalvadorLekan <lekansalvador@gmail.com>
2022-07-12 21:20:31 +0100

    day 9

---
+++ SalvadorLekan/day9.ts
@@ -0,0 +1,30 @@
+function threeSum(nums: number[]): number[][] {
+  const n: number[][] = [];
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue;
+    }
+    let l = i + 1;
+    let r = nums.length - 1;
+    while (l < r) {
+      const sum = nums[i] + nums[l] + nums[r];
+      if (sum === 0) {
+        n.push([nums[i], nums[l], nums[r]]);
+        while (l < r && nums[l] === nums[l + 1]) {
+          l++;
+        }
+        while (l < r && nums[r] === nums[r - 1]) {
+          r--;
+        }
+        l++;
+        r--;
+      } else if (sum < 0) {
+        l++;
+      } else {
+        r--;
+      }
+    }
+  }
+  return n;
+}
