79b43cf063c450dce99138d84f596a39f5dff389
Solomon Ayodele Ogunbowale <solomonayodeleo@gmail.com>
2022-07-13 16:21:58 +0100

    Delete twoSum.py

--- SolomonAyodeleO/Day1/twoSum.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prev = {}
-        for i in range(len(nums)):
-            diff = target - nums[i]
-            if diff in prev:
-                return [i, prev[diff]]
-            prev[nums[i]] = i
