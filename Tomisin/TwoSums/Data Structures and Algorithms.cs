46dba91330e934210c44808cd5b65c7fa68ea498
Tomisin Fagbola <tomisin.fagbola@prunedge.com>
2022-07-15 19:38:21 +0100

    Two Sums

---
+++ Tomisin/TwoSums/Data Structures and Algorithms.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Data_Structures_and_Algorithms
+    {
+
+        public static int[] MaximumNum(int[] nums, int target)
+        {
+            var numsDictionary = new Dictionary<int, int>();
+
+            int complement = 0;
+            for (int i = 0; i < nums.Count(); i++)
+            {
+                complement = target - nums[i];
+                int index = 0;
+                if (numsDictionary.TryGetValue(complement, out index))
+                {
+                    int[] twoSumSolution = { index, i };
+                    return twoSumSolution;
+                }
+
+                if (!numsDictionary.ContainsKey(nums[i]))
+                {
+                    numsDictionary.Add(nums[i], i);
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Tomisin/TwoSums/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp1;
+
+
+
+Console.WriteLine(Data_Structures_and_Algorithms.MaximumNum(new int[] { 2, 7, 11, 15 }, 9));
