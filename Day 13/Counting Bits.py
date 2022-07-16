7f8f8943a7713b9e8c0a9b219f6fc4747f49b699
blayziken <kingtoluwalope@gmail.com>
2022-07-16 21:47:22 +0100

    day 13 done

---
+++ Day 13/Counting Bits.py
@@ -0,0 +1,35 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        
+        array = [0] * (n + 1)
+        offset = 1        
+        
+        for i in range(1, n + 1):
+            if offset * 2 == i:
+                offset = i
+                
+            array[i] = 1 + array[i - offset]
+            
+        return array
+            
+        
+
+            
+#   0:      0 0 0 0
+#   1:      0 0 0 1   1 + dp[n - 1]
+#   2;      0 0 1 0   1 + dp[n - 2]
+#   3:      0 0 1 1   1 + dp[n - 2]
+#   4:      0 1 0 0                   1 + dp[n - 4]
+#   5:      0 1 0 1                   1 + dp[n - 4]
+#   6:      0 1 1 0                   1 + dp[n - 4]
+#   7:      0 1 1 1                   1 + dp[n - 4]
+    
+#   8:      1 0 0 0                   1 + dp[n - 8]
+#   9:      1 0 0 1                   1 + dp[n - 8]
+#   10:     1 0 1 0                   1 + dp[n - 8]
+#   11;     1 0 1 1                   1 + dp[n - 8]
+#   12:     1 1 0 0                   1 + dp[n - 8]
+#   13:     1 1 0 1                   1 + dp[n - 8]
+#   14:     1 1 1 0                   1 + dp[n - 8]
+#   15;     1 1 1 1                   1 + dp[n - 8]
+  
