94d6e4ea71eff99c30fc13007a431a26be3b5074
blayziken <kingtoluwalope@gmail.com>
2022-07-22 22:57:19 +0100

    day 17 done

---
+++ Day 17/Coin Change.py
@@ -0,0 +1,14 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float('inf')] * (amount + 1)
+        
+        # base case
+        dp[0] = 0
+        
+        for i in range(len(dp)):
+            for coin in coins:
+                if i - coin < 0:
+                    continue
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+        
+        return dp[amount] if dp[amount] != float('inf') else -1
