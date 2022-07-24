d99eab8d8d1b1ddc2714676334be2bdcf2f06a00
Oluwafemi <femio82@gmail.com>
2022-07-24 23:08:10 +0100

    a

---
+++ Eff_Emm/lonngest_common_subsequence.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def longestCommonSubsequence(self, text1, text2):
+        """
+        :type text1: str
+        :type text2: str
+        :rtype: int
+        """
+        dp = [[0 for j in range(len(text2)+1)] for i in range(len(text1)+1)]
+        
+        for i in range(len(text1)-1,-1,-1):
+            for j in range(len(text2)-1,-1,-1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i+1][j+1]
+                else:
+                    dp[i][j]  = max(dp[i][j+1],dp[i+1][j])
+        return dp[0][0]
+                    
+        
