6fc18a58c82dd13e16f1e6380891265784f3ac99
Chinwendu20 <amaka013@gmail.com>
2022-07-24 21:47:11 +0100

    Adding solution

---
+++ Maureen(Chinwendum)/longest-increasing-sequence/solution.py
@@ -0,0 +1,18 @@
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        
+        lis=[1]*len(nums)
+        for i in range(len(nums)-1, -1,-1):
+            for j in range(i+1, len(nums)):
+                if nums[i]<nums[j]:
+                    lis[i]=max(lis[i], 1+lis[j])
+        return max(lis)
+        
+   
+        
+        
+        
+        
+        
