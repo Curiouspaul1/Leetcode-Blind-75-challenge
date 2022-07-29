680464bd495b8a1425ad707f75758a3c7b5d907b
Fortune Adekogbe <fortuneadekogbe@gmail.com>
2022-07-29 23:59:21 +0100

    Create decode-ways.py

---
+++ _Enutrof_[__-1]🎧🌝💉☕/Day 24/decode-ways.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp = {len(s): 1}
+        
+        for i in range(len(s)-1,-1, -1):
+            if i in dp:
+                return dp[i]
+            dp[i] = 0 if s[i] == '0' else dp[i+1]
+            
+            if i+1 < len(s) and (s[i]=='1' or s[i]=='2' and s[i+1] in "0123456"):
+                dp[i] += dp[i+2]
+        return dp[0]
