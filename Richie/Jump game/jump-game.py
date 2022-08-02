50e95a1a928daef54d417a5600db832f491fe7cc
RealRichi3 <Molunorichie@gmail.com>
2022-08-02 21:01:16 +0100

    Day 26 - Jump game

---
+++ Richie/Jump game/jump-game.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canJump(self, nums):
+        goal = len(nums) - 1
+
+        for i in range(len(nums) - 1, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+
+        return goal == 0
