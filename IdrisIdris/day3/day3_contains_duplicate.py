776fa4236230006daa5968b9bb3aac557e657c71
Psami-wondah <okechukwusamuel16@gmail.com>
2022-08-19 21:18:50 +0100

    deleted other people werk

--- IdrisIdris/day3/day3_contains_duplicate.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        numSet = set(nums)
-        if len(numSet) != len(nums):
-            return True
-        else:
-            return False
--- St. Camillus De Lelis/solution2.ts
+++
@@ -1,9 +0,0 @@
-// @ts-ignore
-function twoSum(nums: number[], target: number): number[] {
-  for (let num = 0; num < nums.length; num++) {
-    for (let i = num; i < nums.length; i++) {
-      if (nums[num] + nums[i] == target) return [num, i];
-    }
-  }
-  return [];
-}
