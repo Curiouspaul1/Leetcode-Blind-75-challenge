d870d637ebd288802cd8b7e08d21a1ce66059c28
Olixpin <noibiabdulrasaq@gmail.com>
2022-08-19 15:43:39 +0100

    Day-37: Non-overlapping-intervals - done

--- olix/day-37/non-overlapping-interval.js
+++ olix/day-37/non-overlapping-interval.js
@@ -1,1 +1,29 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var eraseOverlapIntervals = function (intervals) {
+  intervals = intervals.sort((a, b) => a[0] - b[0]);
+  let count = 0;
+  let endInterval = intervals[0][1];
 
+  for (let i = 1; i < intervals.length; i++) {
+    let interval = intervals[i];
+    if (interval[0] < endInterval) {
+      count++;
+      endInterval = Math.min(endInterval, interval[1]);
+    } else {
+      endInterval = interval[1];
+    }
+  }
+  return count;
+};
+
+console.log(
+  eraseOverlapIntervals([
+    [1, 2],
+    [2, 3],
+    [3, 4],
+    [1, 3],
+  ])
+);
