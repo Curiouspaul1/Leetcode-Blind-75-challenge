6329a83922aa436040047e07a6179e0576194ed9
Olixpin <noibiabdulrasaq@gmail.com>
2022-08-22 19:56:55 +0100

    Day 40: find-right-interval done

---
+++ olix/day-40/find-right-interval.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[]}
+ */
+var findRightInterval = function (intervals) {
+  let result = [];
+  let sorted = intervals.sort((a, b) => a[0] - b[0]);
+  for (let i = 0; i < intervals.length; i++) {
+    let start = intervals[i][0];
+    let end = intervals[i][1];
+    let index = binarySearch(sorted, end);
+    result.push(index);
+  }
+  return result;
+};
+
+function binarySearch(arr, target) {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    if (arr[mid][0] === target) {
+      return mid;
+    } else if (arr[mid][0] > target) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return -1;
+}
+
+console.log(
+  findRightInterval([
+    [1, 4],
+    [2, 3],
+    [3, 4],
+  ])
+);
