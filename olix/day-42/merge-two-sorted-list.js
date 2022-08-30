443dca76e2a22ee8e4166fc04ab86aa4e589f897
Olixpin <noibiabdulrasaq@gmail.com>
2022-08-30 09:31:29 +0100

    day-42: merge sort

---
+++ olix/day-42/merge-two-sorted-list.js
@@ -0,0 +1,13 @@
+var mergeTwoLists = function (list1, list2) {
+    if (!list1) return list2;
+    if (!list2) return list1;
+    if (list1.val < list2.val) {
+        list1.next = mergeTwoLists(list1.next, list2);
+        return list1;
+    } else {
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2;
+    }
+};
+
+console.log(mergeTwoLists([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]));
