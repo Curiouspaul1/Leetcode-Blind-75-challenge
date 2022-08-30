bffd3d8a94c8c73564658b4d21854b4eebb9c34c
Esosa <64735238+Esosa-AI@users.noreply.github.com>
2022-08-30 23:27:25 +0100

    Create Merge k sorted lists.py

---
+++ Esosa/Day 43- Merge K sorted lists/Merge k sorted lists.py
@@ -0,0 +1,23 @@
+class Solution:
+    def mergeKLists(self, lists):
+        """
+        lists = [[1,4,5],[1,3,4],[2,6]]
+        res = [1,4,5,1,3,4,2,6]
+        """
+
+        heap = []
+        dummy = ListNode()
+        curr = dummy
+
+        for li in lists:
+            while li:
+                heap.append(li.val)
+                li = li.next
+        heapq.heapify(heap)
+
+        while heap:
+            val = heapq.heappop(heap)
+            node = ListNode(val)
+            curr.next = node
+            curr = curr.next
+        return dummy.next
