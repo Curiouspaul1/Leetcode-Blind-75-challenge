03dc6447ba8669395aa03697b9c81dab1d09859c
Psami-wondah <okechukwusamuel16@gmail.com>
2022-09-01 09:21:11 +0100

    day 40

---
+++ psami/Day40 - reverse linked list/solution.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+from typing import List, Optional
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        while head:
+            curr = head
+            head = head.next
+            curr.next = prev
+            prev = curr
+            
+        return prev
