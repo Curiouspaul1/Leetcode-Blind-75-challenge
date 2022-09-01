911dc3bd2d938d0c9cf67452cf67006cbcfde775
Psami-wondah <okechukwusamuel16@gmail.com>
2022-09-01 08:30:04 +0100

    omor This one cheoukwu

--- psami/Day39 - Find Right Interval/solution.py
+++ psami/Day39 - Find Right Interval/solution.py
@@ -1,35 +1,24 @@
+import bisect
 from typing import List
-# x = [[1,4],[2,3],[3,4]]
 class Solution:
-
-
-
     def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
-        sorted = []
-        n = len(intervals)
-        for i in range(n):
-            sorted.append([intervals[i], i])
-        sorted.sort()
+        intervals = sorted((e[0], i, e[1]) for i, e in enumerate(intervals))
+        l = len(intervals)
+        res = [0] * l
+        for e in intervals:
+            r = bisect.bisect_left(intervals, (e[2], ))
+            res[e[1]] = intervals[r][1] if r < l else -1
+        return res
 
-        result = [-1]*n
-        def binary_search(x):
-            if sorted[n-1][0][0] < x : return -1
-            l,r=0,n-1
-            while l<=r:
-                mid = l + (l-r)//2
-                if sorted[mid][0][0] >= x: r =mid -1
-                else: l=mid+1
-            return sorted[l][1]
-        for i in range(n):
-            result[i] =binary_search(intervals[i][1])
-        
-        return result
+
+
+
+
+
+
+
+
+
 
 
     
-
-
-
-
-x = Solution()
-print(5//2)
