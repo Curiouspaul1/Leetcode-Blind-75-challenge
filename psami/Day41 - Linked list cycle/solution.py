0eefb53f4fdbbe55612293d9f27147aeeafef7cc
Psami-wondah <okechukwusamuel16@gmail.com>
2022-09-02 10:00:02 +0100

    day 42

---
+++ psami/Day41 - Linked list cycle/solution.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+from typing import Optional
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
