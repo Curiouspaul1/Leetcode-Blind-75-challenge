4950282a5eef4a7cbb2a9850a6c72bcea432fb75
Richie <molunorichie@gmail.com>
2022-09-07 21:01:14 +0100

    Day 46 - Set matrix

---
+++ Richie/Set matrix/set-matrix.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        R = len(matrix)
+        C = len(matrix[0])
+        rows, cols = set(), set()
+
+        for i in range(R):
+            for j in range(C):
+                if matrix[i][j] == 0:
+                    rows.add(i)
+                    cols.add(j)
+
+        for i in range(R):
+            for j in range(C):
+                if i in rows or j in cols:
+                    matrix[i][j] = 0
