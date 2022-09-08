391c17c52214f954d8749b7822dce7ee97010a07
Psami-wondah <okechukwusamuel16@gmail.com>
2022-09-08 08:22:07 +0100

    day 46

--- psami/Day46 - Set Matrix Zeroes/solution.py
+++ psami/Day46 - Set Matrix Zeroes/solution.py
@@ -19,3 +19,14 @@ class Solution:
                     else:
                         rowZero = True
         
+        for r in range(1,ROWS):
+            for c in range(1,COLS):
+                if matrix[0][c]==0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0
+
+        if matrix[0][0] == 0:
+            for r in range(ROWS):
+                matrix[r][0] = 0
+        if rowZero:
+            for c in range (COLS):
+                matrix[0][c] = 0
