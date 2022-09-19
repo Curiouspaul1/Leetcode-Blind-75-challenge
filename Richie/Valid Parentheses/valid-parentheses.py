c64b6ced0b3a3e1e17d6f1e51a798dff881c7afa
Richie <molunorichie@gmail.com>
2022-09-19 21:15:13 +0100

    Day 54 - Valid Parentheses

---
+++ Richie/Valid Parentheses/valid-parentheses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        open_ch = ["(", "{", "["]
+        closed_ch = [")", "}", "]"]
+        match = {
+            ")": "(",
+            "}": "{",
+            "]": "["
+        }
+        
+        for ch in s:
+            if ch in closed_ch and len(stack) == 0:
+                return False
+            if ch in closed_ch and match[ch] == stack[-1]:
+                stack.pop()
+                continue
+                
+            stack.append(ch)
+        
+        return len(stack) == 0
+        
+        
