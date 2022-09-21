c5ef81f49ea646fd0f8d33d8059357d38de578aa
Timmy Omolana <59034168+Tee-Stark@users.noreply.github.com>
2022-09-22 00:13:54 +0100

    Fifty + six - valid palindrome (updated submission)

--- Engr-T.stark/validPalindrome.cpp
+++ Engr-T.stark/validPalindrome.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string words = "";
-    
-        for(int i=0;i<s.size();i++){
-            if(isalnum(s[i])){
-                words+=tolower(s[i]);
-            }
-        }
-
-        int n = words.size();
+       
+        s.erase(remove_if(s.begin(), s.end(), [](const char &c){ return !isalnum(c); }), s.end());
+       
+        transform(s.begin(), s.end(), s.begin(),[](unsigned char c){ return tolower(c); });
+       
+        int n = s.size();
         int i=0;
         int j=n-1;
 
         while(i<=j){
-            if(words[i]!=words[j]){
+            if(s[i]!=s[j]){
                 return false;
             }
             i++;
@@ -24,3 +21,30 @@ public:
         return true;
     }
 };
+
+// class Solution {
+// public:
+//     bool isPalindrome(string s) {
+//         string words = "";
+    
+//         for(int i=0;i<s.size();i++){
+//             if(isalnum(s[i])){
+//                 words+=tolower(s[i]);
+//             }
+//         }
+
+//         int n = words.size();
+//         int i=0;
+//         int j=n-1;
+
+//         while(i<=j){
+//             if(words[i]!=words[j]){
+//                 return false;
+//             }
+//             i++;
+//             j--;
+//         }
+
+//         return true;
+//     }
+// };
