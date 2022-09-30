077f7c5f35b423b0aa1e742f0c8781e89e69cb50
Psami-wondah <okechukwusamuel16@gmail.com>
2022-09-30 23:11:21 +0100

    Day 59

---
+++ psami/Day59 - Maximum Depth of Binary Tree/solution.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+            
+
+
+        
