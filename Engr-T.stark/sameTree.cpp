4c30139f07de610e65e59589ee07e6467e788a04
Timmy Omolana <timmieomolanakiel@gmail.com>
2022-09-30 23:18:40 +0100

    Sixty - same tree

---
+++ Engr-T.stark/sameTree.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == NULL && q == NULL)
+            return true;
+        
+        if(p != NULL && q != NULL)
+            return (p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
+        return false;
+    }
+};
