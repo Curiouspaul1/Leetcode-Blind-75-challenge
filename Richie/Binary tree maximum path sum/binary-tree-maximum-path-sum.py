54854b4bb775a19548a62ecca459789eb839b1d4
Richie <molunorichie@gmail.com>
2022-10-02 22:48:09 +0100

    Day 62 - Binary Tree Max path sum

---
+++ Richie/Binary tree maximum path sum/binary-tree-maximum-path-sum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        res = [root.val]
+
+        def dfs(root):
+            if root is None:
+                return 0
+            
+            leftMax = dfs(root.left)
+            rightMax = dfs(root.right)
+            leftMax = max(leftMax, 0)
+            rightMax = max(rightMax, 0)
+            
+            res[0] = max(res[0], root.val + leftMax + rightMax)
+            
+            return root.val + max(leftMax, rightMax)
+        
+        dfs(root)
+        
+        return res[0]
