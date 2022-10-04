df885e14d0fb7bada509927382915dd3c2ab187a
Psami-wondah <okechukwusamuel16@gmail.com>
2022-10-04 08:51:32 +0100

    day 61

--- psami/Day59 - Maximum Depth of Binary Tree/solution.py
+++ psami/Day59 - Maximum Depth of Binary Tree/solution.py
@@ -12,7 +12,6 @@ class Solution:
         if not root:
             return 0
 
-        
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
             
 
---
+++ psami/Day61 - Invert Binary Tree Easy  9915  136/solution.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+
+        tmp = root.left
+        root.left=root.right
+        root.right=tmp
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
+
+
+
